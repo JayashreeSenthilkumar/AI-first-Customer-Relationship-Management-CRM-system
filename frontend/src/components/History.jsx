f9d7bbbe562d9f7ceb375f586166de37274b11b7
Jayashree Senthilkumar <146175395+JayashreeSenthilkumar@users.noreply.github.com>
2026-04-23 22:38:51 +0530

    Update History.jsx

--- frontend/src/components/History.jsx
+++ frontend/src/components/History.jsx
@@ -1,1 +1,25 @@
+import { useState } from "react";
+import { getHistory } from "../services/api";
 
+export default function History() {
+  const [name, setName] = useState("");
+  const [data, setData] = useState([]);
+
+  const fetchHistory = async () => {
+    const res = await getHistory(name);
+    setData(res.data.data);
+  };
+
+  return (
+    <div>
+      <h3>History</h3>
+      <input
+        placeholder="HCP Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={fetchHistory}>Fetch</button>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}

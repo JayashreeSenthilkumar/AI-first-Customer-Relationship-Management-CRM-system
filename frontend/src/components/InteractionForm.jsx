324f7d33d3e0b4d0b5b51a6c2d45c6abed029a2f
Jayashree Senthilkumar <146175395+JayashreeSenthilkumar@users.noreply.github.com>
2026-04-23 22:45:52 +0530

    Update InteractionForm.jsx

--- frontend/src/components/InteractionForm.jsx
+++ frontend/src/components/InteractionForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { logForm } from "../services/api";
+import { addInteraction } from "../redux/interactionSlice";
 
 export default function InteractionForm() {
   const [form, setForm] = useState({
@@ -8,22 +10,29 @@ export default function InteractionForm() {
     follow_up: ""
   });
 
+  const dispatch = useDispatch();
+
   const handleSubmit = async () => {
-    await logForm(form);
+    const res = await logForm(form);
+
+    dispatch(addInteraction(res.data));
     alert("Saved!");
   };
 
   return (
     <div>
       <h3>Manual Log</h3>
+
       <input
         placeholder="HCP Name"
         onChange={(e) => setForm({ ...form, hcp_name: e.target.value })}
       />
+
       <input
         placeholder="Topic"
         onChange={(e) => setForm({ ...form, topic: e.target.value })}
       />
+
       <input
         placeholder="Follow Up"
         onChange={(e) => setForm({ ...form, follow_up: e.target.value })}

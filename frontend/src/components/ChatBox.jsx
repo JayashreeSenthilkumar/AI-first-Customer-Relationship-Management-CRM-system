cab32f1dd2b237158d628dcdde68ea558c7a6706
Jayashree Senthilkumar <146175395+JayashreeSenthilkumar@users.noreply.github.com>
2026-04-23 22:45:08 +0530

    Update ChatBox.jsx

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -1,26 +1,47 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { sendChat } from "../services/api";
+import { setChatResponse, setLoading, setError } from "../redux/interactionSlice";
 
 export default function ChatBox() {
   const [message, setMessage] = useState("");
-  const [response, setResponse] = useState(null);
+  const dispatch = useDispatch();
+
+  const { chatResponse, loading } = useSelector(
+    (state) => state.interaction
+  );
 
   const handleSend = async () => {
-    const res = await sendChat(message);
-    setResponse(res.data);
+    try {
+      dispatch(setLoading(true));
+
+      const res = await sendChat(message);
+
+      dispatch(setChatResponse(res.data));
+      dispatch(setLoading(false));
+    } catch (err) {
+      dispatch(setError("Something went wrong"));
+      dispatch(setLoading(false));
+    }
   };
 
   return (
     <div>
       <h3>Chat Log</h3>
+
       <input
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         placeholder="Enter interaction..."
       />
+
       <button onClick={handleSend}>Send</button>
 
-      {response && <pre>{JSON.stringify(response, null, 2)}</pre>}
+      {loading && <p>Loading...</p>}
+
+      {chatResponse && (
+        <pre>{JSON.stringify(chatResponse, null, 2)}</pre>
+      )}
     </div>
   );
 }

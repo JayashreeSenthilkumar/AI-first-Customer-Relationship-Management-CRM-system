d9bdcfffdb9888a62bf10ab72cf3ad34aaa1e70c
Jayashree Senthilkumar <146175395+JayashreeSenthilkumar@users.noreply.github.com>
2026-04-23 22:33:55 +0530

    Update interaction.py

--- backend/app/models/interaction.py
+++ backend/app/models/interaction.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from app.core.database import Base
 
+class Interaction(Base):
+    __tablename__ = "interactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hcp_name = Column(String)
+    topic = Column(String)
+    follow_up = Column(String)
+    summary = Column(String)

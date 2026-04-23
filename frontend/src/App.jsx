f74c0cc038795bc73b5220fbe7b32235e2b28951
Jayashree Senthilkumar <146175395+JayashreeSenthilkumar@users.noreply.github.com>
2026-04-23 22:39:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,16 @@
+import ChatBox from "./components/ChatBox";
+import InteractionForm from "./components/InteractionForm";
+import History from "./components/History";
 
+function App() {
+  return (
+    <div>
+      <h1>AI CRM HCP Module</h1>
+      <ChatBox />
+      <InteractionForm />
+      <History />
+    </div>
+  );
+}
+
+export default App;

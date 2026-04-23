834d5bebbc67ffcb411fd447fb4f0dd16eec26cb
Jayashree Senthilkumar <146175395+JayashreeSenthilkumar@users.noreply.github.com>
2026-04-23 22:38:01 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,12 @@
+import axios from "axios";
 
+const BASE_URL = "http://127.0.0.1:8000";
+
+export const sendChat = (message) =>
+  axios.post(`${BASE_URL}/chat-log`, { message });
+
+export const logForm = (data) =>
+  axios.post(`${BASE_URL}/log-interaction`, data);
+
+export const getHistory = (name) =>
+  axios.get(`${BASE_URL}/history/${name}`);

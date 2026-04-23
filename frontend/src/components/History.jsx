6b7c6ceb5c3439d71ca6a5a2e305e2b1a2da4e05
Jayashree Senthilkumar <146175395+JayashreeSenthilkumar@users.noreply.github.com>
2026-04-23 22:45:28 +0530

    Update History.jsx

--- frontend/src/components/History.jsx
+++ frontend/src/components/History.jsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { getHistory } from "../services/api";
+import { setInteractions } from "../redux/interactionSlice";
 
 export default function History() {
   const [name, setName] = useState("");
-  const [data, setData] = useState([]);
+  const dispatch = useDispatch();
+
+  const { interactions } = useSelector((state) => state.interaction);
 
   const fetchHistory = async () => {
     const res = await getHistory(name);
-    setData(res.data.data);
+    dispatch(setInteractions(res.data.data));
   };
 
   return (
     <div>
       <h3>History</h3>
+
       <input
         placeholder="HCP Name"
         onChange={(e) => setName(e.target.value)}
       />
+
       <button onClick={fetchHistory}>Fetch</button>
 
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(interactions, null, 2)}</pre>
     </div>
   );
 }
